afa6f8fbf947b29cdd1d67dedafa8841fb772e29
Papy181 <papy181@yahoo.fr>
2026-09-20 15:11:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="sisi-sapotay-online-v2";
-const A=["./","./index.html","./admin.html","./client.html","./style.css","./app.js","./config.js","./cloud.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const C="sisi-sapotay-online-v3";
+const A=["./","./index.html","./admin.html","./admin-login.html","./client.html","./style.css","./app.js","./config.js","./cloud.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
